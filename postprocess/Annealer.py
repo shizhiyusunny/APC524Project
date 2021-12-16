035fa5b1f0ce8571433761f1237c8f02e572e0b8
anvitha-s <anvitha@princeton.edu>
2021-12-15 23:55:50 -0500

    Some housekeeping, removing redundant packages and renaming packages

--- postprocess/Annealer.py
+++ postprocess/Annealer.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import numpy as np
 from . import Postprocessing
-from . import ymlReader
 
 class Annealer():
     def ConstantStep(res,f,inputs):
-        n, process=ymlReader.Reader.read(inputs)
-        arr_step=np.linspace(0,1,n)
+        n, process = inputs['AnnealingStep'], inputs['process']
+        arr_step = np.linspace(0,1,n)
         flag_prog=0
         for t in arr_step:
             #res.updatetraction(t)
@@ -20,7 +13,3 @@ class Annealer():
             Postprocessing.Postprocessing.postprocess(t,process,flag_prog)
         flag_prog=1
         Postprocessing.Postprocessing.postprocess(t,process,flag_prog)
-            
-            
-    
-
