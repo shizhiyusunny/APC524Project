7587c4e5cb456bfd42cda58e25ceef0fa2b86c90
shizhiyusunny <90217735+shizhiyusunny@users.noreply.github.com>
2021-12-20 02:59:10 -0500

    Delete shape.py

--- mesh/shape.py
+++
@@ -1,7 +0,0 @@
-class Shape:
-    def build(inputs):
-        from . import rectangle, circle
-        if 'Rectangle' in inputs:
-            return rectangle.Rectangle(inputs['Rectangle'])
-        elif 'Circle' in inputs:
-            return circle.Circle(inputs['Circle'])
