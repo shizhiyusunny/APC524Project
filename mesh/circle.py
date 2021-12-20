27ec75857abbc7cc64352b7d0e690d3bfb365a57
shizhiyusunny <90217735+shizhiyusunny@users.noreply.github.com>
2021-12-20 02:58:57 -0500

    Delete circle.py

--- mesh/circle.py
+++
@@ -1,8 +0,0 @@
-import mshr
-import fenics
-
-class Circle:
-    def __init__(self, inputs):
-        center = fenics.Point(inputs['Center'])
-        radius = inputs['Radius']
-        self.object = mshr.Circle(center, radius)
