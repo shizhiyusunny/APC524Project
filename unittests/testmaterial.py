8ce55520a8ee0962ac39c7771ee66b173d74ca1e
Zhiyu Shi <shizhiyu@LAPTOP-P8A3O7ES.localdomain>
2021-12-21 07:23:52 -0500

    add workflow

---
+++ unittests/testmaterial.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from material import material
+import pytest
+
+
+def test_material1():
+    material_constants = {'E': 0.5, 'nu': 0.3}
+    material.MaterialConstant.processor(material_constants)
+    assert abs(material_constants['mu']-0.1923) < 0.0001
+
+
+def test_material2():
+    material_constants = {'E': 0.6, 'nu': 0.2}
+    material.MaterialConstant.processor(material_constants) 
+    assert abs(material_constants['lambda']-0.125) < 0.0001
+
---
+++ unittests/testmeshgenerator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from fenics import *
+from mshr import *
+from mesh import meshmenerator
+import pytest
+
+
+def test_meshgenerator():
+    meshdict = {'Rectangle':{'Center':'0.0, 0.0', 'Length': 1.0, 'Breadth': 1.0}}
+    meshg = meshmenerator.MeshGenerator(meshdict)
+    mesh1 = meshg.create_mesh_object()
+    domain = Rectangle(Point(-0.5,-0.5),Point(0.5,0.5))
+    mesh2 = generate_mesh(domain,20)
+    assert type(mesh1) == type(mesh2)
+
